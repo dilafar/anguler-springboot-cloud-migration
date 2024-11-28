b5fa611fe02d3c670563079fc06a8caa3f806899
dilafar <it20784720@my.sliit.lk>
2024-11-28 12:44:27 +0530

    azure aks config added

--- employeemanagerfrontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    apiServerUrl: process.env['API_SERVER_URL'],
-  };
--- employeemanagerfrontend/src/environments/environment.ts
+++ employeemanagerfrontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiServerUrl: "http://employee-test.us-east-1.elasticbeanstalk.com/api"
+  apiServerUrl: "http://localhost:8080/api"
 };
