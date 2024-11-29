a206bd8f720fca4afe29e23530373f071687ae05
dilafar <it20784720@my.sliit.lk>
2024-11-29 18:37:49 +0530

    jenkins permission added

---
+++ kubernetes-script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+kubectl apply -f jenkins-setup.yaml
+
+JENKINS_TOKEN=$(kubectl get secret jenkins-token -n employee -o jsonpath='{.data.token}' | base64 --decode)
+
+JENKINS_USER="jenkins"
+CLUSTER_NAME=$(kubectl config view -o jsonpath='{.clusters[0].name}')
+CLUSTER_SERVER=$(kubectl config view -o jsonpath='{.clusters[0].cluster.server}')
+
+kubectl config set-credentials ${JENKINS_USER} --token="${JENKINS_TOKEN}"
+
+kubectl config set-context ${JENKINS_USER}@${CLUSTER_NAME} \
+  --cluster=${CLUSTER_NAME} \
+  --user=${JENKINS_USER} \
+  --namespace=employee
+
+kubectl config use-context ${JENKINS_USER}@${CLUSTER_NAME}
+
+kubectl config current-context
+kubectl get pods -n employee
