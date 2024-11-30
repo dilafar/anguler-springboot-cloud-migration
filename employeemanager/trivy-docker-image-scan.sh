4fd858556f87de5098970c0cfe62034764ea9166
dilafar <it20784720@my.sliit.lk>
2024-11-30 14:12:45 +0530

    trivy image update, cleanup added

--- employeemanager/trivy-docker-image-scan.sh
+++ employeemanager/trivy-docker-image-scan.sh
@@ -5,14 +5,14 @@ echo $dockerImageName
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.57.1 -q image --exit-code 0 --severity HIGH --light $dockerImageName
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.57.1 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
 
-    # Trivy scan result processing
     exit_code=$?
     echo "Exit Code : $exit_code"
 
-    # Check scan results
     if [[ "${exit_code}" == 1 ]]; then
         echo "Image scanning failed. Vulnerabilities found"
-        exit 1;
+        rm -rf $WORKSPACE/.cache/trivy  
+        exit 1
     else
         echo "Image scanning passed. No CRITICAL vulnerabilities found"
+        rm -rf $WORKSPACE/.cache/trivy  
     fi;
