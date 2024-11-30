8ceb65aa279eafb1cbbbcc22b7bab57a7fed4bed
dilafar <it20784720@my.sliit.lk>
2024-11-30 15:27:26 +0530

    trivy image update, cleanup added

--- employeemanager/trivy-docker-image-scan.sh
+++ employeemanager/trivy-docker-image-scan.sh
@@ -2,17 +2,15 @@
 dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 echo $dockerImageName
 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ --user $(id -u jenkins):$(id -g jenkins) aquasec/trivy:0.57.1 -q image --exit-code 0 --severity HIGH --light $dockerImageName
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ --user $(id -u jenkins):$(id -g jenkins) aquasec/trivy:0.57.1 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.57.1 -q image --exit-code 0 --severity HIGH --light $dockerImageName
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.57.1 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
 
     exit_code=$?
     echo "Exit Code : $exit_code"
 
     if [[ "${exit_code}" == 1 ]]; then
         echo "Image scanning failed. Vulnerabilities found"
-        rm -rf /var/lib/jenkins/workspace/my-pipeline/trivy
         exit 1
     else
         echo "Image scanning passed. No CRITICAL vulnerabilities found"
-        rm -rf /var/lib/jenkins/workspace/my-pipeline/trivy 
     fi;
