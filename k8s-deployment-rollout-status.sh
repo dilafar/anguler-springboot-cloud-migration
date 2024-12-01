cd83e2f569438458c545fb61152efd60824109e9
dilafar <it20784720@my.sliit.lk>
2024-12-01 14:23:58 +0530

    opa kubernetes changes added

--- k8s-deployment-rollout-status.sh
+++ k8s-deployment-rollout-status.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-echo "Starting rollout check for deployment 'employee-frontend'..."
 sleep 60s
 
-echo "Running kubectl rollout status..."
-kubectl -n employee rollout status deploy employee-frontend --timeout=30s 2>&1 | tee rollout.log
-
-if ! grep -q "successfully rolled out" rollout.log; then
-    echo "Deployment employee-frontend Rollout has Failed"
-    echo "Rolling back to the previous deployment..."
-    kubectl -n employee rollout undo deploy employee-frontend
-    exit 1
+if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 10s) != *"successfully rolled out"* ]]; 
+then     
+	echo "Deployment ${deploymentName} Rollout has Failed"
+    kubectl -n default rollout undo deploy ${deploymentName}
+    exit 1;
 else
-    echo "Deployment employee-frontend Rollout is Success"
+	echo "Deployment ${deploymentName} Rollout is Success"
 fi
-
