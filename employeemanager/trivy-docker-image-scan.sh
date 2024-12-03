3ea826a76e03301a85e7331249f690c69ff24f55
dilafar <it20784720@my.sliit.lk>
2024-12-03 14:24:32 +0530

    archive artifacts  added

--- employeemanager/trivy-docker-image-scan.sh
+++ employeemanager/trivy-docker-image-scan.sh
@@ -2,8 +2,8 @@
 dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 echo $dockerImageName
 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.57.1 -q image -f json -o trivy.json --exit-code 0 --severity HIGH --light $dockerImageName
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.57.1 -q image -f json -o trivy.json --exit-code 1 --severity CRITICAL --light $dockerImageName
+trivy image -f json -o trivy.json --exit-code 0 --severity HIGH --light $dockerImageName
+trivy image -f json -o trivy.json --exit-code 1 --severity CRITICAL --light $dockerImageName
 
     exit_code=$?
     echo "Exit Code : $exit_code"
--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -2,8 +2,8 @@
 
 echo $1 
 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.57.1 -q image --exit-code 0 --severity HIGH --light $1 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.57.1 -q image --exit-code 1 --severity CRITICAL --light $1 
+trivy image --exit-code 0 --severity HIGH --light $1 
+trivy image --exit-code 1 --severity CRITICAL --light $1 
 
     exit_code=$?
     echo "Exit Code : $exit_code"
