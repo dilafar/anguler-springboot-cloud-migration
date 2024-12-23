b85422243d07b5c369113c5ef4b4954dc68f7a90
dilafar <it20784720@my.sliit.lk>
2024-12-23 14:37:15 +0530

    defectdojo change added

--- employeemanagerfrontend/upload-reports.py
+++ employeemanagerfrontend/upload-reports.py
@@ -6,8 +6,8 @@ scan_type = ''
 
 if file_name == 'njsscan.sarif':
     scan_type = 'SARIF'
-elif file_name == 'semgrep.json':
-    scan_type = 'Semgrep JSON Report'
+#elif file_name == 'semgrep.json':
+#    scan_type = 'Semgrep JSON Report'
 elif file_name == 'retire.json':
     scan_type = 'Retire.js Scan'
 elif file_name == 'dependency-check-report.json':
