0221af3f1adb9b413f90eb7fb900c2249d34c1c1
M.R.A.FADHIL <87592439+dilafar@users.noreply.github.com>
2025-05-04 10:54:03 +0530

    Update trivy-docker-image-scan.sh

--- employeemanager/trivy-docker-image-scan.sh
+++ employeemanager/trivy-docker-image-scan.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
-echo $dockerImageName
-
-#docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.57.1 -q image  --exit-code 0 --severity HIGH --light $dockerImageName 
-#docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.57.1 -q image  --exit-code 1 --severity CRITICAL --light $dockerImageName 
+echo $dockerImageName 
 
 trivy image -f json -o trivy.json --severity HIGH,CRITICAL --exit-code 1 $dockerImageName
 
